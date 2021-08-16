b03667c7cdc62bc98e394ede0bde20ed28a3063f
TheRealJan <jan.m.tennert@gmail.com>
2021-08-17 01:59:22 +0200

    Remove detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,27 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.21"
-    id("io.gitlab.arturbosch.detekt").version("1.18.0")
 }
 
 group = "de.jan.natrium"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven {
-        name = "m2-dv8tion"
-        url = uri("http://m2.dv8tion.net/releases")
-        isAllowInsecureProtocol = true
-    }
+    maven("https://m2.dv8tion.net/releases")
     mavenCentral()
 }
-
-detekt {
-    toolVersion = "1.18.0"
-    config = files("config/detekt/detekt.yml")
-    buildUponDefaultConfig = true
-}
-
 
 dependencies {
     implementation("net.dv8tion:JDA:4.3.0_307")
