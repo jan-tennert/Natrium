75ea48171630397edc532c72b12c4214b93681b6
TheRealJan <jan.m.tennert@gmail.com>
2021-08-17 02:19:02 +0200

    Try to fix jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "de.jan.natrium"
-version = "1.0-SNAPSHOT"
+version = "1.1.2"
 
 plugins {
     kotlin("jvm") version "1.5.21"
@@ -13,6 +13,18 @@ repositories {
     maven("https://m2.dv8tion.net/releases")
     maven("https://jitpack.io")
     mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group.toString()
+            artifactId = "Natrium"
+            version = version
+
+            from(components["kotlin"])
+        }
+    }
 }
 
 detekt {
